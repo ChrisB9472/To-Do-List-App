5c063c5a3750424c7e987e3a40de398b9a13c003
ChrisB9472 <93794387+ChrisB9472@users.noreply.github.com>
2021-11-15 11:30:55 -0500

    Create scripts.js

---
+++ JS/scripts.js
@@ -0,0 +1,27 @@
+function newItem() {
+
+
+    let list = $('<li></li>');
+    let inputValue = $('#input').val()
+    list.append(inputValue);
+  
+    if (inputValue === '') {
+      alert('You must write something')
+    } else {
+      $('#list').append(list)
+    }
+
+    list.on('dblclick', function crossOut() {
+      list.toggleClass('strike');
+    });
+  
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
+    list.append(crossOutButton);
+  
+    crossOutButton.on("click", deleteListItem);
+    function deleteListItem() {
+      list.addClass("delete")
+    }
+    $('#list').sortable();
+  }
